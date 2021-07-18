b42b7741cd162037a8addd1fe194e8baa29afd6b
bennyhuo <bennyhuo@kotliner.cn>
2021-07-18 16:44:17 +0800

    04

---
+++ 04.move_constructor.cpp
@@ -0,0 +1,114 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class Buffer {
+ private:
+  unsigned char *buf;
+  int capacity;
+  int length;
+ public:
+  explicit Buffer(int capacity) : capacity(capacity), length(0), buf(new unsigned char[capacity]{}) {
+    cout << "Constructor: " << *this << endl;
+  }
+
+  //region Copy Construct&Assign
+  Buffer(Buffer &buffer) {
+    cout << "Copy Constructor: " << buffer << endl;
+    this->capacity = buffer.capacity;
+    this->length = buffer.length;
+    this->buf = new unsigned char[buffer.capacity];
+    std::copy(buffer.buf, buffer.buf + buffer.capacity, this->buf);
+  }
+
+  Buffer &operator=(Buffer const &buffer) {
+    cout << "Copy: " << buffer << endl;
+    if (this != &buffer) {
+      this->capacity = buffer.capacity;
+      this->length = buffer.length;
+      delete[] this->buf;
+      this->buf = new unsigned char[buffer.capacity];
+      std::copy(buffer.buf, buffer.buf + buffer.capacity, this->buf);
+    }
+
+    return *this;
+  }
+  //endregion
+
+  //region Move Construct&Assign
+  Buffer(Buffer &&buffer)  noexcept {
+    cout << "Move Constructor: " << buffer << endl;
+    this->capacity = buffer.capacity;
+    this->length = buffer.length;
+    this->buf = buffer.buf;
+
+    buffer.capacity = 0;
+    buffer.length = 0;
+    buffer.buf = nullptr;
+  }
+
+  Buffer &operator=(Buffer &&buffer)  noexcept {
+    cout << "Move: " << buffer << endl;
+    if (this != &buffer) {
+      this->capacity = buffer.capacity;
+      this->length = buffer.length;
+      this->buf = buffer.buf;
+
+      buffer.capacity = 0;
+      buffer.length = 0;
+      buffer.buf = nullptr;
+    }
+
+    return *this;
+  }
+  //endregion
+
+  int GetLength() {
+    return length;
+  }
+
+  int GetCapacity() {
+    return capacity;
+  }
+
+  bool write(unsigned char value) {
+    if (length == capacity) return false;
+    buf[length++] = value;
+    return true;
+  }
+
+  ~Buffer() {
+    cout << "Delete: " << *this << endl;
+    delete[] buf;
+  }
+
+  friend ostream &operator<<(ostream &os, Buffer const &buffer);
+};
+
+ostream &operator<<(ostream &os, Buffer const &buffer) {
+  os << "Buffer(" << buffer.length << "/" << buffer.capacity << ")[";
+  for (int i = 0; i < buffer.capacity; i++) {
+    os << (int) buffer.buf[i] << ", ";
+  }
+  os << "]";
+  return os;
+}
+
+int main() {
+  auto buffer = Buffer(10);
+  buffer = Buffer(16);
+  cout << buffer << endl;
+
+  cout << "------------------" << endl;
+
+  auto buffers = vector<Buffer>();
+  buffers.push_back(Buffer(12));
+  buffers.push_back(Buffer(20));
+  buffers.push_back(Buffer(30));
+  buffers.push_back(Buffer(40));
+  buffers.push_back(Buffer(50));
+  buffers.push_back(Buffer(60));
+  return 0;
+}
--- Readme.md
+++ Readme.md
@@ -6,3 +6,4 @@
 
 - [x] [[编程语言引战] C++ 瞅了瞅 C#：值类型你也要得瑟？](https://www.bilibili.com/video/BV1iM4y1M7Qm/)
 - [x] [[C++] 我的对象可以复制，还可以变着法儿复制](https://www.bilibili.com/video/BV1iM4y1M7Qm/)
+- [x] [[C++] 深复制可真麻烦，那我也不敢说辣鸡 C++](https://www.bilibili.com/video/BV1Wh41167XZ/)
